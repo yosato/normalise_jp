46d25507d13baeb7d318c9bfa220eeb5cc9f5995
U-CERENCE\yo_sato <yo_sato@mar-yosa-01.cerence.net>
2020-02-21 08:41:48 +0100

    some new stuff on cluster homs

--- cluster_homonyms.py
+++ cluster_homonyms.py
@@ -1,8 +1,25 @@
 import homonym_vecs
+import os,sys
+import numpy as np
+
+from scipy.cluster import hierarchy as cluster
 
 def main(HomsMVecs):
     for Hom,OrthsMVecs in HomsMVecs.items():
-        for Orth,MVecs in OrthsMVecs:
-            
+        Clusters=cluster_vecs(OrthsMVecs.values())
+        ClusterSets.append(Clusters)
+    return ClusterSets
+
+def cluster_homonyms(Vecs):
+    cluster.linkage('ward')
+
+
+if __name__=='__main__':
+    import argparse
+    Psr=argparse.ArgumentParser()
+    Psr.add_argument('homsmvecs_path')
+    Args=Psr.parse_args()
+    
+    main(Args.homsvecs_path)
         
         
--- get_orthclassified_words.py
+++ get_orthclassified_words.py
@@ -3,33 +3,17 @@ import count_homophones
 from pythonlib_ys import main as myModule
 imp.reload(count_homophones)
 
-if __name__=='__main__':
-    import argparse,glob,pickle
-    Psr=argparse.ArgumentParser()
-    Psr.add_argument('input_dir')
-    Psr.add_argument('--lemmatise',action='store_true')
-    Psr.add_argument('--tag-type',default='ipa')
-    Psr.add_argument('--filter-in',default=None)
-    Psr.add_argument('--out-fp')
-    Args=Psr.parse_args()
-    MecabCorpusFPs=glob.glob(Args.input_dir+'/*.mecab')
-    if not MecabCorpusFPs:
-        print('stuff does not exist\n')
-        sys.exit()
-
-    GenHomStats=count_homophones.main(MecabCorpusFPs,TagType=Args.tag_type,LemmatiseP=Args.lemmatise)
-
-    def all_hiragana_p(Strs):
+def all_hiragana_p(Strs):
             for Str in Strs:
                 if not myModule.all_of_chartypes_p(Str,['hiragana']):
                     return False
             return True
-    def all_kanjikatakana_contained_p(Strs):
+def all_kanjikatakana_contained_p(Strs):
             for Str in Strs:
                 if not myModule.at_least_one_of_chartypes_p(Str,['han','katakana']):
                     return False
             return True
-    def kanji_hiragana_combo(Strs):
+def kanji_hiragana_combo(Strs):
             if len([Str for Str in Strs if myModule.at_least_one_of_chartypes_p(Str,['han'])])!=1:
                 return False
             if not any(myModule.all_of_chartypes_p(Str,['hiragana']) for Str in Strs):
@@ -38,8 +22,11 @@ if __name__=='__main__':
                 return False
             return True
 
+def filter_and_output(GenHomStats,Criterion='probUnamb'):
+    if Criterion=='probUnamb':
+        filter_and_output_probunamb(GenHomStats)
 
-        
+def filter_and_output_probunamb(GenHomStats):
     OrthAmbStats=[];CatAmbStats=[];UnambStats=[];UniOrthStats=[]    
     for GenHomStat in GenHomStats:
         if len(GenHomStat.superorthsfreqs)==1:
@@ -56,10 +43,31 @@ if __name__=='__main__':
                 else:
                     OrthAmbStats.append(GenHomStat)
         else:
-            CatAmbStats.append(GenHomStat)
+            CatAmbStat2;78;36M2;78;36ms.append(GenHomStat)
 
     Out=open(Args.out_fp,'wt') if Args.out_fp else sys.stdout
         
     SortedUnambStats=sorted(UnambStats,key=lambda a:a.domcatfreq,reverse=True)        
     for UnambStat in SortedUnambStats:
         Out.write(repr(UnambStat.__dict__)+'\n')
+
+
+if __name__=='__main__':
+    import argparse,glob,pickle
+    Psr=argparse.ArgumentParser()
+    Psr.add_argument('input_dir')
+    Psr.add_argument('criterion',default='probUnamb',[])
+    Psr.add_argument('--lemmatise',action='store_true')
+    Psr.add_argument('--tag-type',default='ipa')
+    Psr.add_argument('--filter-in',default=None)
+    Psr.add_argument('--genstats-p',action='store_true')
+    Psr.add_argument('--out-fp')
+    Args=Psr.parse_args()
+    MecabCorpusFPs=glob.glob(Args.input_dir+'/*.mecab')
+    if not MecabCorpusFPs:
+        print('stuff does not exist\n')
+        sys.exit()
+
+    HomStats=count_homophones.main(MecabCorpusFPs,TagType=Args.tag_type,LemmatiseP=Args.lemmatise,GenStatsP.Args.genstats_p)
+
+    filter_and_output(HomStats,Criterion=Args.criterion)
