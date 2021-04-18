cb53005bacc5f995a43de5301c883a137f327c45
Yo Sato <yosato16@gmail.com>
2021-04-18 17:35:25 +0100

    cluster homonyms related scripts

--- clustered_homs/get_clustered_homs.py
+++ clustered_homs/get_clustered_homs.py
@@ -1,10 +1,12 @@
-import normalise_mecab
 import sys,os,imp,glob,pickle
+sys.path.append('..')
+import normalise_mecab
+
 
 imp.reload(normalise_mecab)
 
 def main(FPs):
-    return normalise_mecab.get_clustered_homs_files(FPs,['cat','subcat','subcat2','infpat','pronunciation'],CorpusOrDic='corpus')
+    return normalise_mecab.get_clustered_homs_files(FPs,['cat','subcat','subcat2','infpat','infform','pronunciation'],CorpusOrDic='corpus')
 
 if __name__=='__main__':
     Dir=sys.argv[1]
